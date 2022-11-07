7523923bddea23d0ec1c42b42dd5fbe2e9702255
Ray22XD <117545128+Ray22XD@users.noreply.github.com>
2022-11-06 20:37:03 -0500

    Delete PORCENTAJES.py

--- PORCENTAJES.py
+++
@@ -1,5 +0,0 @@
-h = int(input("ingresa el número de hombres: "))
-m = int(input("ingresa el número de mujeres: "))
-TOTAL = h + m
-print("El porcentaje de hombres es: ", (h/TOTAL)*100, "%")
-print("El porcentaje de mujeres es: ", (m/TOTAL)*100, "%")
