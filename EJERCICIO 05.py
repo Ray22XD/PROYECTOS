d8257bd9703e8e1fab2959e8d00c1adf4db738bd
Ray22XD <117545128+Ray22XD@users.noreply.github.com>
2022-11-06 20:46:41 -0500

    Add files via upload

---
+++ EJERCICIO 05.py
@@ -0,0 +1,9 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+GIGABYTE = float (input ("INGRESE CAPACIDAD GYGABYTE"))
+MEGABYTE = GIGABYTE*1024
+KILOBYTE = GIGABYTE*1048576
+BYTE = GIGABYTE*1073741824
+print(" mostrar MEGABYTE", MEGABYTE,"MB")
+print(" mostrar KILOBYTE", KILOBYTE,"KB")
+print(" mostrar BYTE", BYTE,"BYTE")
