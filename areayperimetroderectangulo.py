54f03dc904b3de6733318ec3a7e3432a7c0a6902
MarcosZV26 <117104689+MarcosZV26@users.noreply.github.com>
2022-11-06 21:02:52 -0500

    Add files via upload

---
+++ areayperimetroderectangulo.py
@@ -0,0 +1,9 @@
+
+base = float(input('Escriba la base del rectángulo: '))
+altura = float(input('Escriba la altura del rectángulo: '))
+
+area = base * altura
+perimetro = 2 * base + 2 * altura
+
+print('El área del rectángulo es: ', area)
+print('El perimetro del rectángulo es: ',perimetro)
