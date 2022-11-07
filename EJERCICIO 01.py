64b745c9c58ea64bdb8b409fbf34ec0ae8f1fafd
Ray22XD <117545128+Ray22XD@users.noreply.github.com>
2022-11-06 20:32:57 -0500

    Add files via upload

---
+++ EJERCICIO 01.py
@@ -0,0 +1,5 @@
+h = int(input("ingresa el número de hombres: "))
+m = int(input("ingresa el número de mujeres: "))
+TOTAL = h + m
+print("El porcentaje de hombres es: ", (h/TOTAL)*100, "%")
+print("El porcentaje de mujeres es: ", (m/TOTAL)*100, "%")
---
+++ EJERCICIO 02.py
@@ -0,0 +1,12 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+METROS =  float (input("INGRESA EL VALOR EN METROS: "))
+CENTIMETROS=METROS*100
+PULGADAS=CENTIMETROS/2.54
+PIES=PULGADAS/12
+YARDA=PIES/3
+print("VALOR DE CENTIMETROS:" + repr(CENTIMETROS))
+print("VALOR DE PIES" + repr(PIES))
+print("VALOR DE PULGADAS:" + repr(PULGADAS))
+print("VALOR DE YARDA:" + repr(YARDA))
+print
