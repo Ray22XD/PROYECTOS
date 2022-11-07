132deb12562cec1b0438494c0833656f3e1228d3
Ray22XD <117545128+Ray22XD@users.noreply.github.com>
2022-11-06 20:36:17 -0500

    Add files via upload

---
+++ EJERCICIO 03.py
@@ -0,0 +1,7 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+kilometros =  float (input("INGRESA EL VALOR EN KILOMETROS: "))
+pies =  float (input("INGRESA EL VALOR EN PIES: "))
+millas=  float (input("INGRESA EL VALOR EN MILLAS: "))
+metros = 1609 *millas + 0.3048 *pies + 1000 *kilometros
+print ("La longitud de ", metros, " metros")
